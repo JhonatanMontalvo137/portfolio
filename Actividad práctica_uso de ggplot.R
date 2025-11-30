90ecbae7f65172363aa77c71e6730b69abfaaec6
Jhonatan <141587649+JhonatanMontalvo137@users.noreply.github.com>
2025-11-30 01:23:45 -0600

    Delete Actividad práctica_uso de ggplot.R

--- Actividad práctica_uso de ggplot.R
+++
@@ -1,8 +0,0 @@
-hotel_bookings <- read.csv("C:/Users/jhona/Downloads/hotel_bookings(3).csv")
-ggplot(data = hotel_bookings) +
-  geom_point(mapping = aes(x = lead_time, y = children))
-
-
-ggplot(data = hotel_bookings) +
-  geom_point(mapping = aes(x = stays_in_weekend_nights, y = children))
-s
